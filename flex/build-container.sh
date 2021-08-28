6ca2e385a25d551cd0ddc149ea6002619415964b
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2021-08-28 20:28:16 +0200

    ♻️ Moved script out of template

--- flex/build-container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo --- Building NEW StaticPagesTestEnv container ---
-docker build -t nginx-test-env:alpine .
-docker run --name StaticPagesTestEnv -d -p 5000:80 nginx-test-env:alpine
-echo --- Done! ---
-exec "$SHELL"
